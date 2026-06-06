ab94da70d52f56995ce24e83212f6fc47a232dc7
hadi-shahid <mhadishahids@gmail.com>
2026-06-06 16:17:43 +0500

    Add files via upload

---
+++ Week # 10/Task # 6.py
@@ -0,0 +1,10 @@
+inp = (input("Enter Any Thing : "))
+print (type(inp))
+
+inp = float(input("Enter Any Number : "))
+print (type(inp))
+
+inp = int(input("Enter Any Number : "))
+print (type(inp))
+
+
